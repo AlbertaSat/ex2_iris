fa60b2a9f0e29ac1122cf04c00eafda71cf6147f
Alex <aepp2@ualberta.ca>
2020-07-07 09:52:28 -0600

    Directly use dcfifo in cmd_cross_clock

--- vhdl/util/cmd_cross_clock.vhd
+++ vhdl/util/cmd_cross_clock.vhd
@@ -1,5 +1,24 @@
+----------------------------------------------------------------
+-- Copyright 2020 University of Alberta
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+----------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
+
+library altera_mf;
+use altera_mf.all;
 
 entity cmd_cross_clock is
 port (
@@ -12,17 +31,32 @@ port (
 end entity cmd_cross_clock;
 
 architecture rtl of cmd_cross_clock is
-    component fifo_1
+    component dcfifo
+    generic (
+        intended_device_family  : string;
+        lpm_numwords            : natural;
+        lpm_showahead           : string;
+        lpm_type                : string;
+        lpm_width               : natural;
+        lpm_widthu              : natural;
+        overflow_checking       : string;
+        rdsync_delaypipe        : natural;
+        read_aclr_synch         : string;
+        underflow_checking      : string;
+        use_eab                 : string;
+        write_aclr_synch        : string;
+        wrsync_delaypipe        : natural
+    );
     port (
-        aclr		: in std_logic;
-        data		: in std_logic_vector(0 downto 0);
-        rdclk		: in std_logic;
-        rdreq		: in std_logic;
-        wrclk		: in std_logic;
-        wrreq		: in std_logic;
-        q		    : out std_logic_vector(0 downto 0);
-        rdempty		: out std_logic;
-        wrfull		: out std_logic 
+        aclr    : in std_logic;
+        data    : in std_logic_vector (0 downto 0);
+        rdclk   : in std_logic;
+        rdreq   : in std_logic;
+        wrclk   : in std_logic;
+        wrreq   : in std_logic;
+        q       : out std_logic_vector (0 downto 0);
+        rdempty : out std_logic;
+        wrfull  : out std_logic 
     );
     end component;
 
@@ -30,19 +64,33 @@ architecture rtl of cmd_cross_clock is
     signal rdempty : std_logic;
     signal wrfull : std_logic;
 begin
+
+    fifo : dcfifo generic map (
+		intended_device_family => "Cyclone V",
+		lpm_numwords => 4,
+		lpm_showahead => "ON",
+		lpm_type => "dcfifo",
+		lpm_width => 1,
+		lpm_widthu => 2,
+		overflow_checking => "ON",
+		rdsync_delaypipe => 4,
+		read_aclr_synch => "OFF",
+		underflow_checking => "ON",
+		use_eab => "ON",
+		write_aclr_synch => "OFF",
+		wrsync_delaypipe => 4
+	) port map (
+		aclr => not reset_n,
+		data(0) => i,
+		rdclk => o_clock,
+		rdreq => not rdempty,
+		wrclk => i_clock,
+		wrreq => i and not wrfull,
+		q(0) => q,
+		rdempty => rdempty,
+		wrfull => wrfull  -- TODO: if this is ever high, raise an error or something
+	);
     
     o <= q and not rdempty;
 
-    cmd_fifo : fifo_1 port map (
-        aclr => not reset_n,
-        data(0) => i,
-        rdclk => o_clock,
-        rdreq => not rdempty,
-        wrclk => i_clock,
-        wrreq => i and not wrfull,
-        q(0) => q,
-        rdempty => rdempty,
-        wrfull => wrfull  -- TODO: if this is ever high, raise an error or something
-    );
-    
 end architecture rtl;
