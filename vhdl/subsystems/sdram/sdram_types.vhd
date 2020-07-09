72b0e288a6c2b26712e01c7cae6a817c0fd8dbc4
Nash Gould <nggould@ualberta.ca>
2020-07-09 15:55:52 -0600

    Fixed some of the types, made the header a record

--- vhdl/subsystems/sdram/sdram_types.vhd
+++ vhdl/subsystems/sdram/sdram_types.vhd
@@ -17,13 +17,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.fpga_types.all;
 
 package sdram_types is
-    type sdram_config_t is record
-        memory_base     : integer;
-        memory_bounds   : integer;
-    end record sdram_config_t;
-
     subtype sdram_address_block_t is array(0 to 1) of integer;
 
     type sdram_error_t is (SDRAM_NO_ERROR, SDRAM_FULL, SDRAM_MPU_CHECK_FAILED);
@@ -45,12 +41,21 @@ package sdram_types is
         vnir_temp_base      : integer;
         vnir_temp_bounds    : integer;
     end record sdram_config_out_t;
-
-    type sdram_config is record
-        to_sdram : sdram_config_to_sdram_t;
-        from_sdram : sdram_config_from_sdram_t;
-    end record sdram_config;
     
-    subtype sdram_header is std_logic_vector (159 downto 0)
-
+    type sdram_header_t is record
+        timestamp        : timestamp_t;
+        user_defined     : std_logic_vector (7 downto 0);
+        x_size           : integer range 0 to 2^16 - 1;
+        y_size           : integer range 0 to 2^16 - 1;
+        z_size           : integer range 0 to 2^16 - 1;
+        sample_type      : std_logic;
+        reserved_1       : std_logic_vector (1 downto 0);
+        dyna_range       : integer;
+        sample_encode    : std_logic;
+        interleave_depth : std_logic (15 downto 0);
+        reserved_2       : std_logic_vector (1 downto 0);
+        output_word      : integer;
+        entropy_coder    : std_logic;
+        reserved_3       : std_logic_vector (9 downto 0);
+    end record sdram_header_t;
 end package sdram_types;
