2dd1861f75ea669e4003f3376a5bb561d847e373
Nash Gould <nggould@ualberta.ca>
2020-07-10 11:29:22 -0600

    Did some work to make sure everything compiles

--- vhdl/subsystems/sdram/header_creator.vhd
+++ vhdl/subsystems/sdram/header_creator.vhd
@@ -42,31 +42,35 @@ entity header_creator is
         vnir_rows       : in integer;
         swir_rows       : in integer;
 
-        --Flag indicating the headers have been sent
-        headers_sent    : out std_logic;
-        sending_img     : in std_logic;
+        --Flag indicating the imager is still working
+        sending_img     : in std_logic
     );
 end entity header_creator;
 
 architecture rtl of header_creator is
     -- A function to turn a header record into a std_logic_vector for output
-    function sdram_header_to_vec(header : sdram_header_t) return std_logic_vector (159 downto 0) is
+    function sdram_header_to_vec(header : sdram_header_t) return std_logic_vector is
+	variable buffer_vec : std_logic_vector (159 downto 0);
     begin
-        return (159 downto 96 => std_logic_vector(header.timestamp),
-                95 downto 88 => header.user_defined,
-                87 downto 72 => std_logic_vector(unsigned(header.x_size, 16)),
-                71 downto 56 => std_logic_vector(unsigned(header.y_size, 16)),
-                55 downto 40 => std_logic_vector(unsigned(header.z_size, 16)),
-                39           => header.sample_type;
-                38 downto 37 => header.reserved_1;
-                36 downto 33 => std_logic_vector(header.dyna_range);
-                32           => header.sample_encode;
-                31 downto 16 => header.interleave_depth;
-                15 downto 14 => header.reserved_2;
-                13 downto 11 => std_logic_vector(header.output_word);
-                10           => header.entropy_coder;
-                9 downto 0   => header.reserved_3);
+        buffer_vec := std_logic_vector(header.timestamp) &
+                header.user_defined &
+                std_logic_vector(to_unsigned(header.x_size, 16)) &
+                std_logic_vector(to_unsigned(header.y_size, 16)) &
+                std_logic_vector(to_unsigned(header.z_size, 16)) &
+                header.sample_type &
+                header.reserved_1 &
+                std_logic_vector(to_unsigned(header.dyna_range, 4)) &
+                header.sample_encode &
+                header.interleave_depth &
+                header.reserved_2 &
+                std_logic_vector(to_unsigned(header.output_word, 3)) &
+                header.entropy_coder &
+                header.reserved_3;
+	return buffer_vec;
     end function sdram_header_to_vec;
+    
+    --A flag indicating that the header has been sent
+    signal headers_sent : std_logic := '0';
     
 begin
     main_process : process (clock) is
@@ -75,52 +79,81 @@ begin
         variable vnir_header : sdram_header_t;
 
         --Flag to represet the previous header has been sent and a new one is not needed
-        variable new_header_needed : std_logic := 1;
+        variable new_header_needed : std_logic := '1';
     begin
 
         if rising_edge(clock) then
             if (reset_n = '0') then
                 --Assigning all values to 0
-                swir_header.user_defined, vnir_header.user_defined := (others => '0');
-                swir_header.x_size, vnir_header.x_size := 0;
-                swir_header.y_size, vnir_header.y_size := 0;
-                swir_header.z_size, vnir_header.z_size := 0; 
-                swir_header.sample_type, vnir_header.sample_type := '0';
-                swir_header.reserved_1, vnir_header.reserved_1 := "00";
-                swir_header.dyna_ragne, vnir_header.dyna_range := 0;
-                swir_header.sample_encode, vnir_header.sample_encode := '0';
-                swir_header.interleave_depth, vnir_header.interleave_depth := (others => '0');
-                swir_header.reserved_2, vnir_header.reserved_2 := "00";
-                swir_header.output_word, vnir_header.output_word := 0;
-                swir_header.entropy_coder, vnir_header.entropy_coder := '0';
-                swir_header.reserved_3, vnir_header.reserved_3 := (others => '0');
+                swir_header.timestamp           := (others => '0');
+                swir_header.user_defined        := (others => '0');
+                swir_header.x_size              := 0;
+                swir_header.y_size              := 0;
+                swir_header.z_size              := 0; 
+                swir_header.sample_type         := '0';
+                swir_header.reserved_1          := (others => '0');
+                swir_header.dyna_range          := 0;
+                swir_header.sample_encode       := '0';
+                swir_header.interleave_depth    := (others => '0');
+                swir_header.reserved_2          := (others => '0');
+                swir_header.output_word         := 0;
+                swir_header.entropy_coder       := '0';
+                swir_header.reserved_3          := (others => '0');
+                
+                vnir_header.timestamp           := (others => '0');
+                vnir_header.user_defined        := (others => '0');
+                vnir_header.x_size              := 0;
+                vnir_header.y_size              := 0;
+                vnir_header.z_size              := 0;
+                vnir_header.sample_type         := '0';
+                vnir_header.reserved_1          := (others => '0');
+                vnir_header.dyna_range          := 0;
+                vnir_header.sample_encode       := '0';
+                vnir_header.interleave_depth    := (others => '0');
+                vnir_header.reserved_2          := (others => '0');
+                vnir_header.output_word         := 0;
+                vnir_header.entropy_coder       := '0';
+                vnir_header.reserved_3          := (others => '0');
                
-            elsif (new_header_needed == '1') then
-                if (vnir_header.user_defined(0) == '0' or vnir_header.user_defined(0) == 'U') then
+            elsif (new_header_needed = '1') then
+                if (vnir_header.user_defined(0) = '0' or vnir_header.user_defined(0) = 'U') then
                     --Assigning shared values to both headers
-                    swir_header.user_defined, vnir_header.user_defined := (0 => 1, others => '0');
-                    swir_header.sample_encode, vnir_header.sample_encode := '1';
-                    swir_header.interleave_depth, vnir_header.interleave_depth := (others => '0');
-                    swir_header.reserved_2, vnir_header.reserved_2 := "00";
-                    swir_header.output_word, vnir_header.output_word := 1;
-                    swir_header.entropy_coder, vnir_header.entropy_coder := '0';
-                    swir_header.reserved_3, vnir_header.reserved_3 := (others => '0');
+                    swir_header.user_defined        := (0 => '1', others => '0');
+                    swir_header.sample_type         := '0';
+                    swir_header.reserved_1          := (others => '0');
+                    swir_header.sample_encode       := '1';
+                    swir_header.interleave_depth    := (others => '0');
+                    swir_header.reserved_2          := "00";
+                    swir_header.output_word         := 1;
+                    swir_header.entropy_coder       := '0';
+                    swir_header.reserved_3          := (others => '0');
+
+                    vnir_header.user_defined        := (0 => '1', others => '0');
+                    vnir_header.sample_type         := '0';
+                    vnir_header.reserved_1          := (others => '0');
+                    vnir_header.sample_encode       := '1';
+                    vnir_header.interleave_depth    := (others => '0');
+                    vnir_header.reserved_2          := "00";
+                    vnir_header.output_word         := 1;
+                    vnir_header.entropy_coder       := '0';
+                    vnir_header.reserved_3          := (others => '0');
 
                     --Assigning values for differences between swir & vnir
-                    swir_header.timestamp := to_unsigned(0, swir_header.timestamp'length)
-                    swir_header.x_size := 512;
-                    swir_header.y_size := 0;
-                    swir_header.z_size := 1;
-                    swir_header.dyna_range := 16;
+                    swir_header.timestamp   := to_unsigned(0, swir_header.timestamp'length);
+                    swir_header.x_size      := 512;
+                    swir_header.y_size      := 0;
+                    swir_header.z_size      := 1;
+                    swir_header.dyna_range  := 16;
 
-                    vnir_header.timestamp := to_unsigned(0, vnir_header.timestamp'length)
-                    vnir_header.x_size := 2048;
-                    vnir_header.y_size := 0;
-                    vnir_header.z_size := 3;
-                    vnir_header.dyna_range := 10;
+                    vnir_header.timestamp   := to_unsigned(0, vnir_header.timestamp'length);
+                    vnir_header.x_size      := 2048;
+                    vnir_header.y_size      := 0;
+                    vnir_header.z_size      := 3;
+                    vnir_header.dyna_range  := 10;
 
-                elsif (to_integer(timestamp) /= 0 and swir_rows /= 0 and vnir_rows /= 0 and headers_sent == '0') then
-                    swir_header.timestamp, vnir_header.timestamp := timestamp;
+                elsif (to_integer(timestamp) /= 0 and swir_rows /= 0 and vnir_rows /= 0 and headers_sent = '0') then
+                    swir_header.timestamp := timestamp;
+                    vnir_header.timestamp := timestamp;
                     swir_header.y_size := swir_rows;
                     vnir_header.y_size := vnir_rows;
 
@@ -132,17 +165,18 @@ begin
                 
                 -- Sneaky way of ensuring that the header isn't overwritten while waiting for the command
                 -- creator to send back the sending_img signal
-                elsif (sending_img == '1') then
-                    new_header_needed == '0';
+                elsif (sending_img = '1') then
+                    new_header_needed := '0';
                     vnir_img_header <= (others => '0');
                     swir_img_header <= (others => '0');
                 end if;
 
             else
-                if sending_img == '0' then
+                if (sending_img = '0') then
                     new_header_needed := '1';
                     headers_sent <= '0';
-                    swir_header.user_defined, vnir_header.user_defined := (others => '0');
+                    swir_header.user_defined := (others => '0');
+                    vnir_header.user_defined := (others => '0');
                 end if;
             end if;
         end if;
