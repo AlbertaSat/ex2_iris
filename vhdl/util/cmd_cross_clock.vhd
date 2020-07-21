6e983e1085289a841f3be56f01a83f8381cc7e3f
Alex <aepp2@ualberta.ca>
2020-07-21 16:47:19 -0600

    Add reset output in cmd_cross_clock

--- vhdl/util/cmd_cross_clock.vhd
+++ vhdl/util/cmd_cross_clock.vhd
@@ -22,11 +22,12 @@ use altera_mf.all;
 
 entity cmd_cross_clock is
 port (
-    reset_n : in std_logic;
-    i_clock : in std_logic;
-    i       : in std_logic;
-    o_clock : in std_logic;
-    o       : out std_logic
+    reset_n   : in std_logic;
+    i_clock   : in std_logic;
+    i         : in std_logic;
+    o_clock   : in std_logic;
+    o         : out std_logic;
+    o_reset_n : out std_logic
 );
 end entity cmd_cross_clock;
 
@@ -49,18 +50,19 @@ architecture rtl of cmd_cross_clock is
     );
     port (
         aclr    : in std_logic;
-        data    : in std_logic_vector (0 downto 0);
+        data    : in std_logic_vector;
         rdclk   : in std_logic;
         rdreq   : in std_logic;
         wrclk   : in std_logic;
         wrreq   : in std_logic;
-        q       : out std_logic_vector (0 downto 0);
+        q       : out std_logic_vector;
         rdempty : out std_logic;
         wrfull  : out std_logic 
     );
     end component;
 
-    signal q : std_logic;
+    signal data : std_logic_vector(1 downto 0);
+    signal q : std_logic_vector(1 downto 0);
     signal rdempty : std_logic;
     signal wrfull : std_logic;
 begin
@@ -70,7 +72,7 @@ begin
 		lpm_numwords => 4,
 		lpm_showahead => "ON",
 		lpm_type => "dcfifo",
-		lpm_width => 1,
+		lpm_width => 2,
 		lpm_widthu => 2,
 		overflow_checking => "ON",
 		rdsync_delaypipe => 4,
@@ -81,16 +83,20 @@ begin
 		wrsync_delaypipe => 4
 	) port map (
 		aclr => not reset_n,
-		data(0) => i,
+        data => data,
 		rdclk => o_clock,
 		rdreq => not rdempty,
 		wrclk => i_clock,
 		wrreq => i and not wrfull,
-		q(0) => q,
+		q => q,
 		rdempty => rdempty,
 		wrfull => wrfull  -- TODO: if this is ever high, raise an error or something
 	);
     
-    o <= q and not rdempty;
+    data(0) <= i;
+    data(1) <= reset_n;
+
+    o <= q(0) and not rdempty;
+    o_reset_n <= q(1) or rdempty;
 
 end architecture rtl;
