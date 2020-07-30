9709bc689c50f310f0e4d123f9de056216d1fd10
Nash Gould <nggould@ualberta.ca>
2020-07-30 12:05:02 -0600

    Added signals specifying the start and end of an image

--- vhdl/subsystems/sdram/sdram_utils/partition_register.vhd
+++ vhdl/subsystems/sdram/sdram_utils/partition_register.vhd
@@ -18,13 +18,16 @@ entity partition_register is
         bounds_write, filled_add, filled_subtract : in std_logic;
 
         --Values to write
-        base, bounds, add_sub_length : in unsigned(31 downto 0);
+        base, bounds, add_sub_length : in sdram_address;
 
         --Partition read from the register
         part_out : out partition_t;
 
+        --Unsigneds representing the new image
+        img_start, img_end : out sdram_address;
+
         --Error signals
-        busy, full, not_init, bad_mpu_check : out std_logic
+        full, bad_mpu_check : out std_logic
     );
 end entity partition_register;
 
@@ -36,11 +39,14 @@ architecture rtl of partition_register is
     signal buffer_part : partition_t;
 
     --These signals are combinational, and can be written to at any time
-    signal buffer_base, buffer_bounds : unsigned(31 downto 0);
-    signal buffer_fill_base, buffer_fill_bounds : unsigned(31 downto 0);
+    signal buffer_base, buffer_bounds : sdram_address;
+    signal buffer_fill_base, buffer_fill_bounds : sdram_address;
+
+    --A flag saying the partition cannot be added to
+    signal no_add : std_logic;
 
     --Checks if the image will overflow the position, returns 1 if it does
-    function check_overflow(partition : partition_t, address_size : unsigned(31 downto 0)) return std_logic is
+    function check_overflow(partition : partition_t, address_size : sdram_address) return std_logic is
     begin
         if (partition.filled_bounds + address_size > partition.bounds) then
             return '1';
@@ -50,7 +56,7 @@ architecture rtl of partition_register is
     end function check_overflow;
     
     --Checks if the image won't fit in the memory because it is too full, returns 1 if it does
-    function check_full(partition : partition_t, address_size : unsigned(31 downto 0), start_address : unsigned(31 downto 0)) return std_logic is
+    function check_full(partition : partition_t, address_size : sdram_address, start_address : sdram_address) return std_logic is
     begin
         if (start_address <= partition.fill_base and start_address + address_size >= partition.fill_base) then
             return '1';
@@ -59,17 +65,16 @@ architecture rtl of partition_register is
         end if;
     end function check_full;
 begin
-    state_process : process(state, add_sub_length, base, bounds) is
+    --Combinitorial process specifying signal assignments
+    state_process : process(state, add_sub_length, base, bounds, add_sub_length) is
     begin
         case state is
             when init =>
                 buffer_base <= base;
                 buffer_bounds <= bounds;
-            when idle =>
-                buffer_base <= buffer_part.base;
-                buffer_bounds <= buffer_part.bounds;
-                buffer_fill_base <= buffer_part.fill_base;
-                buffer__fill_base <= buffer_part.fill_bounds;
+
+            when idle => null;
+
             --When adding stuff to the memory
             when add_address =>
                 --Some complicated conditional checking (yay!)
@@ -77,19 +82,27 @@ begin
                     if (check_overflow(buffer_part, add_sub_length) = '1') then
                         if (check_full(buffer_part, add_sub_length, buffer_part.base) = '1') then
                             full <= '1';
+                            no_add = '1';
                         else
                             buffer_fill_bounds <= buffer_part.base + add_sub_length;
+                            img_start <= buffer_part.base;
+                            img_end <= buffer_part.base + add_sub_length;
                         end if;
                     else
                         --No need to check if it's full, the overflow did that already
                         buffer_fill_bounds <= buffer_part.fill_bounds + add_sub_length;
+                        img_start <= buffer_part.fill_bounds;
+                        img_end <= buffer_part.fill_bounds + add_sub_length;
                     end if;
                 else
                     --Just need to check if it's full for this case
                     if (check_full(buffer_part, add_sub_length, buffer_part.fill_bounds) = '1') then
                         full = '1';
+                        no_add = '1';
                     else
-                        buffer_fill_bounds <= buffer_part.fill+bounds + add_sub_length;
+                        buffer_fill_bounds <= buffer_part.fill + bounds + add_sub_length;
+                        img_start <= buffer_part.fill_bounds;
+                        img_end <= buffer_part.fill_bounds + add_sub_length;
                     end if;
                 end if;
 
@@ -100,21 +113,19 @@ begin
                 else
                     buffer_fill_base <= buffer_part.fill_base + add_sub_length;
                 end if;
+
+                if (no_add = '1') then
+                    no_add <= '0';
+                    full <= '0';
+                end if;
         end case;
     end process;
-    
-    state_clocking : process (clk, reset_n) is
-    begin
-        if (reset_n = '0') then
-            state <= init;
-        elsif rising_edge(clk) then
-            state <= next_state;
-        end if;
-    end process;
 
+    --Assigns outputs at the rising edge of the clock
     sig_assign : process (clk, reset_n) is
     begin
         if (reset_n = '0') then
+            state <= init;
             buffer_part.base <= (others => '0');
             buffer_part.bounds <= (others => '0');
             buffer_part.fill_base <= (others => '0');
@@ -126,6 +137,7 @@ begin
             part_out.fill_bounds <= (others => '0');
 
         elsif rising_edge(clk) then
+            state <= next_state;
             buffer_part.base <= buffer_base;
             buffer_part.bounds <= buffer_bounds;
             buffer_part.fill_base <= buffer_fill_base;
@@ -138,6 +150,7 @@ begin
         end if;
     end process;
 
+    --Describes when to change the states
     state_change : process (clk, reset_n) is
     begin
         case state is
@@ -148,7 +161,7 @@ begin
                     next_state <= init;
                 end if;
             when idle =>
-                if (filled_add = '1') then
+                if (filled_add = '1' and no_add /= '1') then
                     next_state <= add_address;
                 elsif (filled_subtract = '1') then
                     next_state <= sub_address;
@@ -156,10 +169,10 @@ begin
                     next_state <= idle;
                 end if;
             when add_address =>
-                --Takes on clock cycle to do the things
+                --Will only take one clock cycle
                 next_state <= idle;
             when sub_address =>
-                --Same thing ass add_address
+                --This should only take one clock cycle
                 next_state <= idle;
         end case;
     end process;
