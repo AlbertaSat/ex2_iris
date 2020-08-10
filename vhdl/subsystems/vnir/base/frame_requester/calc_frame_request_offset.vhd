80ebb4fb093062e070167d2ea3da177951424ee6
Alex <aepp2@ualberta.ca>
2020-08-10 11:54:57 -0600

    Use LPM_DIVIDE

--- vhdl/subsystems/vnir/base/frame_requester/calc_frame_request_offset.vhd
+++ vhdl/subsystems/vnir/base/frame_requester/calc_frame_request_offset.vhd
@@ -54,9 +54,40 @@ port (
     offset          : out integer
 );
 end entity calc_frame_request_offset;
-    
+
 
 architecture rtl of calc_frame_request_offset is
+
+    component idivide is
+    generic (
+        N_CLOCKS : integer := 4
+    );
+    port (
+        clock   : in std_logic;
+        reset_n : in std_logic;
+        n       : in integer;
+        d       : in integer;
+        q       : out integer;
+        start   : in std_logic;
+        done    : out std_logic
+    );
+    end component idivide;
+
+
+    component imultiply is
+    generic (
+        N_CLOCKS : integer := 1
+    );
+    port (
+        clock   : in std_logic;
+        reset_n : in std_logic;
+        a       : in integer;
+        b       : in integer;
+        p       : out integer;
+        start   : in std_logic;
+        done    : out std_logic
+    );
+    end component imultiply;
 
     constant CLOCKS_PER_SCLOCKS : real := real(CLOCKS_PER_SEC) / real(SCLOCKS_PER_SEC);
     constant EXTRA_EXPOSURE_CLOCKS : integer := integer(
@@ -66,179 +97,30 @@ architecture rtl of calc_frame_request_offset is
         (20.0 + 2.0 * 16.0 / real(FRAGMENT_WIDTH)) * CLOCKS_PER_SCLOCKS
     );
 
-    constant CLOCKS_TO_COMPUTE : integer := 5;
-
-    signal fps_in : integer;
-    signal exposure_time_in : integer;
-
     signal clocks_per_frame : integer;
+    signal done_clocks_per_frame : std_logic;
+    
     signal clocks_per_exposure : integer;
-    signal offset_out : integer;
-
-    attribute altera_attribute : string;
-    attribute altera_attribute of rtl : architecture is "-name SDC_STATEMENT ""set_multicycle_path -from [get_registers *calc_frame_request_offset:*_in*] -to [get_registers *calc_frame_request_offset:*offset*] -setup -end 4; set_multicycle_path -from [get_registers *calc_frame_request_offset:*_in*] -to [get_registers *calc_frame_request_offset:*offset*] -hold -end 4""";
-
+    signal done_clocks_per_exposure : std_logic;
+    
 begin
 
-    clocks_per_frame <= CLOCKS_PER_SEC / fps_in;
-    clocks_per_exposure <= CLOCKS_PER_SEC * exposure_time_in / 1000;
-    offset_out <= clocks_per_exposure - EXTRA_EXPOSURE_CLOCKS;
-    
-    process
-        type state_t is (RESET, IDLE, CALCULATING);
-        variable state : state_t;
+    calc_clocks_per_frame : idivide port map (
+        clock => clock, reset_n => reset_n,
+        n => CLOCKS_PER_SEC, d => fps,
+        q => clocks_per_frame,
+        start => start, done => done_clocks_per_frame
+    );
 
-        variable t : integer;
-    begin
-        wait until rising_edge(clock);
+    calc_clocks_per_exposure : idivide port map (
+        clock => clock, reset_n => reset_n,
+        n => CLOCKS_PER_SEC * exposure_time, d => 1000,
+        q => clocks_per_exposure,
+        start => done_clocks_per_frame, done => done_clocks_per_exposure
+    );
 
-        done <= '0';
 
-        if reset_n = '0' then
-            state := RESET;
-        end if;
-
-        case state is
-        when RESET => state := IDLE;
-        when IDLE =>
-            if start = '1' then
-                t := 0;
-                fps_in <= fps;
-                exposure_time_in <= exposure_time;
-                state := CALCULATING;
-            end if;
-        when CALCULATING =>
-            if t = CLOCKS_TO_COMPUTE then
-                state := IDLE;
-                offset <= offset_out;
-                done <= '1';
-            end if;
-            t := t + 1;
-        end case;
-    
-    end process;
-
+    offset <= clocks_per_exposure - EXTRA_EXPOSURE_CLOCKS;
+    done <= done_clocks_per_exposure;
 
 end architecture rtl;
-
-
-
-
-
-
--- entity calc_frame_request_offset is
--- generic (
---     CLOCKS_PER_SEC  : integer;
---     SCLOCKS_PER_SEC : integer;
---     FRAGMENT_WIDTH  : integer
--- );
--- port (
---     clock           : in std_logic;
---     reset_n         : in std_logic;
-
---     fps             : in integer;
---     exposure_time   : in integer;
-
---     start           : in std_logic;
---     done            : out std_logic;
-
---     offset          : out integer
--- );
--- end entity calc_frame_request_offset;
-
--- architecture rtl of calc_frame_request_offset is
-
---     constant CLOCKS_PER_SCLOCKS : real := real(CLOCKS_PER_SEC) / real(SCLOCKS_PER_SEC);
---     constant EXTRA_EXPOSURE_CLOCKS : integer := integer(
---         129.0 * 0.43 * 20.0 * CLOCKS_PER_SCLOCKS
---     );
---     constant FOT_CLOCKS : integer := integer(
---         (20.0 + 2.0 * 16.0 / real(FRAGMENT_WIDTH)) * CLOCKS_PER_SCLOCKS
---     );
-
---     signal done_p0 : std_logic;
---     signal fps_p0 : integer;
---     signal exposure_time_p0 : integer;
-
---     signal done_p1 : std_logic;
---     --signal exposure_time_p1 : integer;
---     signal clocks_per_frame_p1 : integer;
-
---     signal done_p2 : std_logic;
---     signal clocks_per_exposure_p2 : integer;
---     --signal clocks_per_frame_p2 : integer;
-
---     signal done_p3 : std_logic;
---     signal frame_request_offset_p3 : integer;
---     --signal clocks_per_frame_p3 : integer;
-
--- begin
-
---     p0 : process
---     begin
---         wait until rising_edge(clock);
---         done_p0 <= '0';
---         if reset_n /= '0' and start = '1' then
---             fps_p0 <= fps;
---             exposure_time_p0 <= exposure_time;
---             done_p0 <= '1';
---         end if;
---     end process p0;
-
-
---     p1 : process
---     begin
---         wait until rising_edge(clock);
---         done_p1 <= '0';
---         if reset_n /= '0' and done_p0 = '1' then
---             clocks_per_frame_p1 <= CLOCKS_PER_SEC / fps_p0;
---             --exposure_time_p1 <= exposure_time_p0;
---             done_p1 <= '1';
---         end if;
---     end process p1;
-
---     p2 : process
---     begin
---         wait until rising_edge(clock);
---         done_p2 <= '0';
---         if reset_n /= '0' and done_p1 = '1' then
---             clocks_per_exposure_p2 <= CLOCKS_PER_SEC * exposure_time_p0 / 1000;
---             --clocks_per_frame_p2 <= clocks_per_frame_p1;
---             done_p2 <= '1';
---         end if;
---     end process p2;
-
---     p3 : process
---     begin
---         wait until rising_edge(clock);
---         done_p3 <= '0';
---         if reset_n /= '0' and done_p2 = '1' then
---             frame_request_offset_p3 <= clocks_per_exposure_p2 - EXTRA_EXPOSURE_CLOCKS;
---             --clocks_per_frame_p3 <= clocks_per_frame_p2;
---             done_p3 <= '1';
---         end if;
---     end process p3;
-
---     p4 : process
---     begin
---         wait until rising_edge(clock);
---         done <= '0';
---         if reset_n /= '0' and done_p3 = '1' then
-            
---             offset <= frame_request_offset_p3;
---             done <= '1';
-
---             if frame_request_offset_p3 <= 0 then
---                 report "Can't compute frame_request_offset: requested exposure is too low" severity failure;
---                 offset <= 1;
---             end if;
-            
---             if frame_request_offset_p3 + FOT_CLOCKS > clocks_per_frame_p1 then
---                 report "Can't compute frame_request_offset: requested exposure is too high" severity failure;
---                 offset <= clocks_per_frame_p1 - FOT_CLOCKS;
---             end if;
-
---         end if;
---     end process p4;
-
--- end architecture rtl;
---
+++ vhdl/util/math.vhd
@@ -0,0 +1,175 @@
+----------------------------------------------------------------
+-- Copyright 2020 University of Alberta
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+----------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library lpm;
+use lpm.lpm_components.all;
+
+
+entity idivide is
+generic (
+    N_CLOCKS : integer := 4
+);
+port (
+    clock   : in std_logic;
+    reset_n : in std_logic;
+    n       : in integer;
+    d       : in integer;
+    q       : out integer;
+    start   : in std_logic;
+    done    : out std_logic
+);
+end entity idivide;
+
+
+architecture rtl of idivide is
+
+    component LPM_DIVIDE is 
+    generic (
+        lpm_drepresentation : string := "SIGNED";
+        lpm_hint            : string := "MAXIMIZE_SPEED=9,LPM_REMAINDERPOSITIVE=TRUE";
+        lpm_nrepresentation : string := "SIGNED";
+        lpm_pipeline        : natural := N_CLOCKS;
+        lpm_type            : string := "LPM_DIVIDE";
+        lpm_widthd          : natural := 32;
+        lpm_widthn          : natural := 32
+    );
+    port (
+        clock       : in std_logic;
+        aclr        : in std_logic;
+        numer       : in std_logic_vector(lpm_widthn-1 downto 0);
+        denom       : in std_logic_vector(lpm_widthd-1 downto 0);
+        quotient    : out std_logic_vector(lpm_widthn-1 downto 0)
+    );
+    end component LPM_DIVIDE;
+
+    component n_delay
+    generic (
+        DELAY_CLOCKS : integer := N_CLOCKS
+    );
+    port (
+        clock   : in std_logic;
+        reset_n : in std_logic;
+        i       : in std_logic;
+        o       : out std_logic
+    );
+    end component n_delay;
+    
+
+    signal q_logic : std_logic_vector(31 downto 0);
+
+begin
+
+    compute_quotient : LPM_DIVIDE port map (
+        clock => clock,
+        aclr => not reset_n,
+        numer => std_logic_vector(to_signed(n, 32)),
+        denom => std_logic_vector(to_signed(n, 32)),
+        quotient => q_logic
+    );
+    q <= to_integer(signed(q_logic));
+
+    delay : n_delay port map (
+        clock => clock,
+        reset_n => reset_n,
+        i => start,
+        o => done
+    );
+
+end architecture rtl;
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library lpm;
+use lpm.lpm_components.all;
+
+
+entity imultiply is
+generic (
+    N_CLOCKS : integer := 1
+);
+port (
+    clock   : in std_logic;
+    reset_n : in std_logic;
+    a       : in integer;
+    b       : in integer;
+    p       : out integer;
+    start   : in std_logic;
+    done    : out std_logic
+);
+end entity imultiply;
+
+
+architecture rtl of imultiply is
+
+    component LPM_MULT is 
+    generic (
+        lpm_hint            : string := "MAXIMIZE_SPEED=9";
+        lpm_pipeline        : natural := N_CLOCKS;
+        lpm_representation : string := "SIGNED";
+        lpm_type            : string := "LPM_MULT";
+        lpm_widtha          : natural := 32;
+        lpm_widthb          : natural := 32;
+        lpm_widthp          : natural := 32
+    );
+    port (
+        clock       : in std_logic;
+        aclr        : in std_logic;
+        dataa       : in std_logic_vector(lpm_widtha-1 downto 0);
+        datab       : in std_logic_vector(lpm_widthb-1 downto 0);
+        result      : out std_logic_vector(lpm_widthp-1 downto 0)
+    );
+    end component LPM_MULT;
+
+    component n_delay
+    generic (
+        DELAY_CLOCKS : integer := N_CLOCKS
+    );
+    port (
+        clock   : in std_logic;
+        reset_n : in std_logic;
+        i       : in std_logic;
+        o       : out std_logic
+    );
+    end component n_delay;
+    
+    signal p_logic : std_logic_vector(31 downto 0);
+
+begin
+
+    compute_product : LPM_MULT port map (
+        clock => clock,
+        aclr => not reset_n,
+        dataa => std_logic_vector(to_signed(a, 32)),
+        datab => std_logic_vector(to_signed(b, 32)),
+        result => p_logic
+    );
+    p <= to_integer(signed(p_logic));
+
+    delay : n_delay port map (
+        clock => clock,
+        reset_n => reset_n,
+        i => start,
+        o => done
+    );
+
+end architecture rtl;
